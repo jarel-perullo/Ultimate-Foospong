9791363afdbb8e392b6760533cf77bcf0861853e
Jarel Perullo <drperullo@gmail.com>
2018-09-23 14:43:32 -0400

    Initial commit

---
+++ Assets/Scripts/BallMove.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallMove : MonoBehaviour {
+
+	public float force = 100;
+
+	// Use this for initialization
+	void Start() {
+		gameObject.GetComponent<Rigidbody2D>().AddForce((Vector2.left + Vector2.up) * force);
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+	}
+}
