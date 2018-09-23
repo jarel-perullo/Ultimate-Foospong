3b857f6be6fe33b993dacd5e443230d2198428ce
Jarel Perullo <drperullo@gmail.com>
2018-09-23 19:20:23 -0400

    Added more stuff

---
+++ Assets/Scripts/MovePaddles.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePaddles : MonoBehaviour {
+
+	public float moveSpeed = 0.015f;
+
+
+	void Update() {
+		float move = Input.GetAxis("Vertical") * moveSpeed;
+		transform.Translate(move * Vector2.up);
+	}
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		Debug.Log("Yup, you did you a collision.");
+		if (collision.gameObject.CompareTag("Wall")) {
+			GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+		}
+	}
+
+}
