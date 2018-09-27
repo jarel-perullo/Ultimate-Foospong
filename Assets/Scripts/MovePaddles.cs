1d8cd24a0615ef8bfabdd5ca2b43690b700fe96b
Jarel Perullo <drperullo@gmail.com>
2018-09-27 16:23:43 -0400

    Did more stuff.  Yup.  Super descriptive.

--- Assets/Scripts/MovePaddles.cs
+++ Assets/Scripts/MovePaddles.cs
@@ -4,19 +4,38 @@ using UnityEngine;
 
 public class MovePaddles : MonoBehaviour {
 
-	public float moveSpeed = 0.015f;
+	public float moveSpeed = 10f;
+
+	public GameObject leftPaddle;
+	public GameObject rightPaddle;
 
 
 	void Update() {
-		float move = Input.GetAxis("Vertical") * moveSpeed;
-		transform.Translate(move * Vector2.up);
+		UpdateLeftPaddle();
+		UpdateRightPaddle();
+
+	}
+
+	private void UpdateLeftPaddle() {
+		leftPaddle.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+		if (Input.GetKey(KeyCode.W)) {
+			leftPaddle.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, moveSpeed);
+		} else if (Input.GetKey(KeyCode.S)) {
+			leftPaddle.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, -moveSpeed);
+		}
+	}
+
+	private void UpdateRightPaddle() {
+		rightPaddle.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			rightPaddle.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, moveSpeed);
+		} else if (Input.GetKey(KeyCode.DownArrow)) {
+			rightPaddle.GetComponent<Rigidbody2D>().velocity = new Vector2(0f, -moveSpeed);
+		}
 	}
 
 	void OnCollisionEnter2D(Collision2D collision) {
-		Debug.Log("Yup, you did you a collision.");
-		if (collision.gameObject.CompareTag("Wall")) {
-			GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-		}
+
 	}
 
 }
